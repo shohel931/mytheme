06f46b5e3e447a5779b0ce1151b4527ddf0ac39d
MD. Shohel Rana <shohel065931@gmail.com>
2025-04-07 14:20:44 +0600

    hv

--- template/home_page.php
+++ template/home_page.php
@@ -9,13 +9,15 @@ get_header(); ?>
 <section id="hero_area">
     <div class="container">
         <div class="row">
-            <div class="col-sm-6">
-                <h2><?php echo get_field(''); ?> Welcome to Our Website</h2>
-                <p><?php echo get_field(''); ?>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br> Quisque at erat nec nisi facilisis bibendum.</p>
-                <a href="#" class="btn btn-primary"><?php echo get_field(''); ?>Get Started</a>
+            <div class="col-sm-5">
+                <h2><?php echo get_field('hero_area_title'); ?></h2>
+                <p><?php echo get_field('hero_area_title_description'); ?></p>
+                <br>
+                <a href="#" class="btn btn-primary"><?php echo get_field('hero_area_button'); ?></a>
             </div>
-            <div class="col-sm-6">
-                <img src="<?php echo get_template_directory_uri(); ?>/img/image1.png" alt="Hero Image" class="img-fluid">
+            <div class="col-sm-7">
+                <?php $image= get_field('hero_area_image'); ?>
+                <img src="<?php echo $image; ?>" alt="">
             </div>
         </div>
     </div>
@@ -28,33 +30,36 @@ get_header(); ?>
             <div class="col-sm-4">
                 <div class="box">
                     <div class="sub_box">
-                        <img src="<?php echo get_template_directory_uri(). '/img/icon.png'  ?>" alt="">
+                    <?php $image= get_field('section_2_box_1_image'); ?>
+                    <img src="<?php echo $image; ?>" alt="">
                     </div>
                     <div class="sub_box">
-                        <h2><?php echo get_field(''); ?>15k+</h2>
-                        <p><?php echo get_field(''); ?>Active user</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-sm-4">
-            <div class="box">
-                    <div class="sub_box">
-                        <img src="<?php echo get_template_directory_uri(). '/img/icon2.png'  ?>" alt="">
-                    </div>
-                    <div class="sub_box">
-                        <h2><?php echo get_field(''); ?>15k+</h2>
-                        <p><?php echo get_field(''); ?>Active Now</p>
+                        <h2><?php echo get_field('section_2_box_1_title'); ?></h2>
+                        <p><?php echo get_field('section_2_box_1_description'); ?></p>
                     </div>
                 </div>
             </div>
             <div class="col-sm-4">
             <div class="box">
                     <div class="sub_box">
-                        <img src="<?php echo get_template_directory_uri(). '/img/icon3.png'  ?>" alt="">
+                    <?php $image= get_field('section_2_box_2_image_'); ?>
+                    <img src="<?php echo $image; ?>" alt="">
                     </div>
                     <div class="sub_box">
-                        <h2><?php echo get_field(''); ?>15k+</h2>
-                        <p><?php echo get_field(''); ?>Active Now</p>
+                        <h2><?php echo get_field('section_2_box_2_title'); ?></h2>
+                        <p><?php echo get_field('section_2_box_2_description'); ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+            <div class="box">
+                    <div class="sub_box">
+                    <?php $image= get_field('section_2_box_3_image'); ?>
+                    <img src="<?php echo $image; ?>" alt="">
+                    </div>
+                    <div class="sub_box">
+                        <h2><?php echo get_field('section_2_box_3_title'); ?></h2>
+                        <p><?php echo get_field('section_2_box_3_description_'); ?></p>
                     </div>
                 </div>
             </div>
@@ -67,17 +72,18 @@ get_header(); ?>
     <div class="container">
         <div class="row">
             <div class="col-sm-5">
-                <h2><?php echo get_field(''); ?>We Are Focus On <br> Your Ultimate Goal</h2>
-                <p><?php echo get_field(''); ?>An enim nullam tempor sapien gravida donec enim ipsum porta justo congue magna at pretium</p>
+                <h2><?php echo get_field('section_3_title'); ?></h2>
+                <p><?php echo get_field('section_3_description'); ?></p>
 
                 <div class="box_area">
                     <div class="main_sub_box">
                     <div class="sub_box_area">
-                        <img src="<?php echo get_template_directory_uri(). '/img/icon4.png'  ?>" alt="">
+                    <?php $image= get_field('section_3_box_1_icon'); ?>
+                    <img src="<?php echo $image; ?>" alt="">
                     </div>
                     <div class="sub_box_area">
-                        <h4><?php echo get_field(''); ?>Automatic Time Tracking</h4>
-                        <p><?php echo get_field(''); ?>Time Tracking is never been easier.</p>
+                        <h4><?php echo get_field('section_3_box_1_title'); ?></h4>
+                        <p><?php echo get_field('section_3_box_1_description'); ?></p>
                     </div>
                     </div>
                 </div>
@@ -85,11 +91,12 @@ get_header(); ?>
                 <div class="box_area">
                 <div class="main_sub_box">
                     <div class="sub_box_area">
-                    <img src="<?php echo get_template_directory_uri(). '/img/icon5.png'  ?>" alt="">
+                    <?php $image= get_field('section_3_box_2_icon_'); ?>
+                    <img src="<?php echo $image; ?>" alt="">
                     </div>
                     <div class="sub_box_area">
-                    <h4><?php echo get_field(''); ?>Automatic Time Tracking</h4>
-                    <p><?php echo get_field(''); ?>Time Tracking is never been easier.</p>
+                    <h4><?php echo get_field('section_3_box_2_title_'); ?></h4>
+                    <p><?php echo get_field('section_3_box_2_description'); ?></p>
                     </div>
                     </div>
                 </div>
@@ -97,11 +104,12 @@ get_header(); ?>
                 <div class="box_area">
                 <div class="main_sub_box">
                     <div class="sub_box_area">
-                    <img src="<?php echo get_template_directory_uri(). '/img/icon6.png'  ?>" alt="">
+                    <?php $image= get_field('section_3_box_3_icon'); ?>
+                    <img src="<?php echo $image; ?>" alt="">
                     </div>
                     <div class="sub_box_area">
-                    <h4><?php echo get_field(''); ?>Automatic Time Tracking</h4>
-                    <p><?php echo get_field(''); ?>Time Tracking is never been easier.</p>
+                    <h4><?php echo get_field('section_3_box_3_title_'); ?></h4>
+                    <p><?php echo get_field('section_3_box_3_description'); ?></p>
                     </div>
                     </div>
                 </div>
