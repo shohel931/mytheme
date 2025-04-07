d85f2f7ad36ec8ad61022bc83b6806dedd5e8747
MD. Shohel Rana <shohel065931@gmail.com>
2025-04-08 05:45:02 +0600

    ht

--- footer.php
+++ footer.php
@@ -1,12 +1,15 @@
-<?php
-/**
- * Template Name: Footer Area
- */
-
-?>
 
 <section id="area_all">
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="area_all_content">
+                    <h3><?php echo get_field(''); ?>Subscribe To Our Newsletter & Get The <br> Coupon Code.</h3>
+                    <p><?php echo get_field(''); ?>All your information is completely confidential</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
 
