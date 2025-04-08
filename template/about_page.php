2736e7af82350d41d50c087abef088672491864b
MD. Shohel Rana <shohel065931@gmail.com>
2025-04-08 20:36:36 +0600

    yj

--- template/about_page.php
+++ template/about_page.php
@@ -60,18 +60,35 @@ get_header(); ?>
     <div class="container">
         <div class="row">
             <div class="col-sm-5">
+            <?php $image= get_field(''); ?>
+            <img src="<?php echo $image; ?>" alt="">
             <img src="<?php echo get_template_directory_uri(). '/img/about1.png' ?>" alt="">
             </div>
             <div class="col-sm-7">
             <h2><?php echo get_field(''); ?>We Are Un 6 Years <br> Experience In This Field</h2>
             <p><?php echo get_field(''); ?>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-            <div class="google_box">
-            <img src="<?php echo get_template_directory_uri(). '/img/abicon.png' ?>" alt="">
-            <h4><?php echo get_field(''); ?>Get Insights Only Google Can Give</h4>
-            </div>
-            <div class="google_box">
-            <img src="<?php echo get_template_directory_uri(). '/img/abicon.png' ?>" alt="">
-            <h4><?php echo get_field(''); ?>Get Insights Only Google Can Give</h4>
+           
+            <div class="main_box_area">
+                <div class="sub_box_area">
+                    <div class="icon_box">
+                    <?php $image= get_field(''); ?>
+                    <img src="<?php echo $image; ?>" alt="">
+                    <img src="<?php echo get_template_directory_uri(). '/img/abicon.png' ?>" alt="">
+                    </div>
+                    <div class="google_box">
+                    <h4><?php echo get_field(''); ?>Get Insights Only Google Can Give</h4>
+                    </div>
+                </div>
+                <div class="sub_box_area">
+                    <div class="icon_box">
+                    <?php $image= get_field(''); ?>
+                    <img src="<?php echo $image; ?>" alt="">
+                    <img src="<?php echo get_template_directory_uri(). '/img/abicon.png' ?>" alt="">
+                    </div>
+                    <div class="google_box">
+                    <h4><?php echo get_field(''); ?>Get Insights Only Google Can Give</h4>
+                    </div>
+                </div>
             </div>
             </div>
         </div>
