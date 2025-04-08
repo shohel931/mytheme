8f8f72531b775df81f17d8df7c2d36c3185da60c
MD. Shohel Rana <shohel065931@gmail.com>
2025-04-08 20:12:53 +0600

    dfg

--- template/about_page.php
+++ template/about_page.php
@@ -10,13 +10,13 @@ get_header(); ?>
     <div class="container">
         <div class="row">
             <div class="col-sm-6">
-                <h2><?php echo get_field(''); ?>Working Closely With <br> Customers In Our <br> MYMT </h2>
-                <p><?php echo get_field(''); ?>An enim nullam tempor sapien gravida donec enim <br> ipsum porta justo congue magna at</p>
+                <h2><?php echo get_field('hero_area_title'); ?></h2>
+                <p><?php echo get_field('hero_area_description'); ?></p>
                 <br>
-                <a href="#"><?php echo get_field(''); ?>Get Start Now</a>
+                <a href="#"><?php echo get_field('hero_area_button'); ?></a>
             </div>
-            <div class="col-sm-6">
-                <img src="<?php echo get_template_directory_uri(). '/img/about.png' ?>" alt="">
+            <div class="col-sm-6"><?php $image= get_field('hero_area_image'); ?>
+            <img src="<?php echo $image; ?>" alt="">
             </div>
         </div>
     </div>
@@ -29,8 +29,8 @@ get_header(); ?>
     <div class="container">
         <div class="row">
             <div class="col-sm-5">
-            <h2><?php echo get_field(''); ?>We Are Un 6 Years <br> Experience In This Field </h2>
-            <p><?php echo get_field(''); ?>An enim nullam tempor sapien gravida donec enim <br> ipsum porta justo congue magna at</p>
+            <h2><?php echo get_field('section_2_title'); ?></h2>
+            <p><?php echo get_field('section_2_description'); ?></p>
             <div class="slider">
           <div>I am a slide.</div>
          <div>I am another slide.</div>
@@ -39,12 +39,12 @@ get_header(); ?>
             </div>
             <div class="col-sm-7">
                 <div class="area_box">
-                    <h2 class="number" data-target="<?php echo get_field(''); ?>">15k</h2>
-                    <p><?php echo get_field(''); ?>Happy Customer</p>
+                    <h2 class="number" data-target="<?php echo get_field('section_2_box_1_title'); ?>"></h2>
+                    <p><?php echo get_field('section_2_box_1_description'); ?></p>
                 </div>
                 <div class="area_box">
-                <h2class="number" data-target="<?php echo get_field(''); ?>">400+</h2class=>
-                <p><?php echo get_field(''); ?>Complete Work</p>
+                <h2 class="number" data-target="<?php echo get_field('section_2_box_2_title_'); ?>"></h2>
+                <p><?php echo get_field('section_2_box_2_description_'); ?></p>
                 </div>
             </div>
         </div>
@@ -59,13 +59,13 @@ get_header(); ?>
 <section id="section_area3">
     <div class="container">
         <div class="row">
-            <div class="col-sm-7">
-            <img src="<?php echo get_template_directory_uri(). '/img/about.png' ?>" alt="">
+            <div class="col-sm-5">
+            <img src="<?php echo get_template_directory_uri(). '/img/about1.png' ?>" alt="">
             </div>
-            <class="col-sm-5">
+            <div class="col-sm-7">
             <h2><?php echo get_field(''); ?>We Are Un 6 Years <br> Experience In This Field</h2>
             <p><?php echo get_field(''); ?>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-            <div class="google_box">
+            <div class="google_box*+-">
             <img src="<?php echo get_template_directory_uri(). '/img/.png' ?>" alt="">
             <h4><?php echo get_field(''); ?>Get Insights Only Google Can Give</h4>
             </div>
