fd667318695452f8c3b971f93d5e3f7da91d7359
MD. Shohel Rana <shohel065931@gmail.com>
2025-04-10 12:20:23 +0600

    ghh

--- template/blog_page.php
+++ template/blog_page.php
@@ -10,15 +10,14 @@ get_header(); ?>
     <div class="container">
         <div class="row">
             <div class="col-sm-5">
-                <h2><?php echo get_field(''); ?>Analytics & Reporting <br> Service For Your <br> Business</h2>
-                <p><?php echo get_field(''); ?>An enim nullam tempor sapien gravida donec enim <br> ipsum porta justo congue magna at</p>
+                <h2><?php echo get_field('title'); ?></h2>
+                <p><?php echo get_field('description'); ?></p>
                 <br>
-                <a href="#"><?php echo get_field(''); ?>Get Start Now</a>
+                <a href="#"><?php echo get_field('button'); ?></a>
             </div>
             <div class="col-sm-5">
-                <?php $image= get_field(''); ?>
+                <?php $image= get_field('image'); ?>
                 <img src="<?php echo $image; ?>" alt="">
-                <img src="<?php echo get_template_directory_uri(). '/img/about.png' ?>" alt="">
             </div>
         </div>
     </div>
