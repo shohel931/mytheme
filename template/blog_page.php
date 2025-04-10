4306dd65284d2e06809625dd0e4d5022787c7389
MD. Shohel Rana <shohel065931@gmail.com>
2025-04-10 12:28:48 +0600

    fgh

--- template/blog_page.php
+++ template/blog_page.php
@@ -25,7 +25,29 @@ get_header(); ?>
 
 
 
-
+<section id="blog_area2">
+    <div class="blog_title1">
+        <h3><?php echo get_field(''); ?></h3>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-5">
+            <?php $image= get_field(''); ?>
+            <img src="<?php echo $image; ?>" alt="">
+                <img src="<?php echo get_template_directory_uri(). '/img/png.png' ?>" alt="">
+                <h3><?php echo get_field(''); ?></h3>
+                <p><?php echo get_field(''); ?></p>
+            </div>
+            <div class="col-sm-5">
+            <?php $image= get_field(''); ?>
+            <img src="<?php echo $image; ?>" alt="">
+                <img src="<?php echo get_template_directory_uri(). '/img/png.png' ?>" alt="">
+                <h3><?php echo get_field(''); ?></h3>
+                <p><?php echo get_field(''); ?></p>
+            </div>
+        </div>
+    </div>
+</section>
 
 
 
