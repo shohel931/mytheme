4881bc0ca3efa8c62a645cb7f5bd0804e3f40be9
MD. Shohel Rana <shohel065931@gmail.com>
2025-04-10 12:11:52 +0600

    yjfg

--- template/blog_page.php
+++ template/blog_page.php
@@ -9,11 +9,11 @@ get_header(); ?>
 <section id="blog_hero">
     <div class="container">
         <div class="row">
-            <div class="col-sm-7">
-                <h2><?php echo get_field(''); ?></h2>
-                <p><?php echo get_field(''); ?></p>
+            <div class="col-sm-5">
+                <h2><?php echo get_field(''); ?>Analytics & Reporting <br> Service For Your <br> Business</h2>
+                <p><?php echo get_field(''); ?>An enim nullam tempor sapien gravida donec enim <br> ipsum porta justo congue magna at</p>
                 <br>
-                <a class="btn btn-primary" href="#"><?php echo get_field(''); ?></a>
+                <a href="#"><?php echo get_field(''); ?>Get Start Now</a>
             </div>
             <div class="col-sm-5">
                 <?php $image= get_field(''); ?>
