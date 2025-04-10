8e093a423d8c280639cf7bca0446876d86d4b845
MD. Shohel Rana <shohel065931@gmail.com>
2025-04-10 12:44:16 +0600

    fgh

--- template/blog_page.php
+++ template/blog_page.php
@@ -30,22 +30,21 @@ get_header(); ?>
     <div class="blog_title1">
         <h3><?php echo get_field(''); ?>Look What Our Working <br> Process</h3>
     </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-5">
-            <?php $image= get_field(''); ?>
+
+    <div class="blog_main_bx">
+        <div class="sub_blog_bx">
+        <?php $image= get_field(''); ?>
             <img src="<?php echo $image; ?>" alt="">
                 <img src="<?php echo get_template_directory_uri(). '/img/png.png' ?>" alt="">
                 <h4><?php echo get_field(''); ?>Our Co-Founders Tuomo Riekki And Kristo</h4>
                 <p><?php echo get_field(''); ?>Our Co-Founders Tuomo Riekki And Kristo Ovaska are serial entrepreneurs who, back in 2013.</p>
-            </div>
-            <div class="col-sm-5">
-            <?php $image= get_field(''); ?>
+        </div>
+        <div class="sub_blog_bx">
+        <?php $image= get_field(''); ?>
             <img src="<?php echo $image; ?>" alt="">
                 <img src="<?php echo get_template_directory_uri(). '/img/png.png' ?>" alt="">
                 <h4><?php echo get_field(''); ?>Our Co-Founders Tuomo Riekki And Kristo</h4>
                 <p><?php echo get_field(''); ?>Our Co-Founders Tuomo Riekki And Kristo Ovaska are serial entrepreneurs who, back in 2013.</p>
-            </div>
         </div>
     </div>
 </section>
