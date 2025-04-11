433c3f228b79dff22e36920b48d13726c2bfa18c
MD. Shohel Rana <shohel065931@gmail.com>
2025-04-11 07:31:25 +0600

    gf

--- template/blog_page.php
+++ template/blog_page.php
@@ -90,4 +90,40 @@ get_header(); ?>
 
 
 
+
+<section id="service_area4">
+    <div class="serv_title">
+    <h3><?php echo get_field('title'); ?></h3>
+    </div>
+    <div class="serv_main_bx">
+        <div class="sub_serv_bx">
+        <?php $image= get_field('box_1_image'); ?>
+        <img src="<?php echo $image; ?>" alt="">
+            <h4><?php echo get_field('box_1_title'); ?></h4>
+            <p><?php echo get_field('box_1_description'); ?></p>
+        </div>
+        <div class="sub_serv_bx">
+        <?php $image= get_field('box_2_image'); ?>
+        <img src="<?php echo $image; ?>" alt="">
+            <h4><?php echo get_field('box_2_title_'); ?></h4>
+            <p><?php echo get_field('box_2_description'); ?></p>
+        </div>
+        <div class="sub_serv_bx">
+        <?php $image= get_field('box_3_image'); ?>
+        <img src="<?php echo $image; ?>" alt="">
+            <h4><?php echo get_field('box_3_title_'); ?></h4>
+            <p><?php echo get_field('box_3_description'); ?></p>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
 <?php get_footer(); ?>
