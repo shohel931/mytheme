7015b5dc64c2cde8cb65dd64f943947e23015eed
MD. Shohel Rana <shohel065931@gmail.com>
2025-04-12 12:13:52 +0600

    g

--- footer.php
+++ footer.php
@@ -24,37 +24,6 @@
 
 
 
-<div id="demo" class="carousel slide" data-bs-ride="carousel">
-
-  <!-- Indicators/dots -->
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
-    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
-    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
-  </div>
-  
-  <!-- The slideshow/carousel -->
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="bg.png" alt="bg" class="d-block" style="width:100%">
-    </div>
-    <div class="carousel-item">
-      <img src="chicago.jpg" alt="Chicago" class="d-block" style="width:100%">
-    </div>
-    <div class="carousel-item">
-      <img src="ny.jpg" alt="New York" class="d-block" style="width:100%">
-    </div>
-  </div>
-  
-  <!-- Left and right controls/icons -->
-  <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
-    <span class="carousel-control-next-icon"></span>
-  </button>
-</div>
-
 
 
 
@@ -83,7 +52,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-6">
-                    <a href="#">Tram's & Condition</a>
+                    <a href="#"><?php echo get_option('coppy'); ?></a>
                 </div>
                 <div class="col-sm-6">
                     <p><?php echo get_theme_mod('theme_copyright_section'); ?></p>
--- inc/settings.php
+++ inc/settings.php
@@ -1,12 +1,16 @@
 <?php 
 
 function settings_api() {
-add_settings_field('coppy', 'Coppyright'. 'coppy_func', 'genarel');
+add_settings_field('coppy', 'Coppyright', 'coppy_func', 'media');
+
+register_setting('media','coppy');
 
 }
 add_action('admin_init','settings_api');
 
 
 function coppy_func(){
-    echo 'Hello World';
+    ?>
+    <input type="text" name="coppy" value="<?php echo get_option('coppy'); ?>" placeholder="Coppyright" class="regular-text" />
+    <?php
 }
