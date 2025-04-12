89536ac72b562305860337f8e50871d88523af61
MD. Shohel Rana <shohel065931@gmail.com>
2025-04-12 17:32:03 +0600

    gj

--- inc/settings.php
+++ inc/settings.php
@@ -1,27 +1,5 @@
 <?php 
 
-function settings_api() {
-add_settings_field('footer', 'Footer', 'footer_func', 'media');
-add_settings_field('coppy', 'Coppyright', 'coppy_func', 'media');
-
-register_setting('media','footer');
-register_setting('media','coppy');
-
-}
-add_action('admin_init','settings_api');
-
-
-function footer_func(){
-    ?>
-    <input type="text" name="footer" value="<?php echo get_option('footer'); ?>" placeholder="Footer" class="regular-text" />
-    <?php
-}
-
-function coppy_func(){
-    ?>
-    <input type="text" name="coppy" value="<?php echo get_option('coppy'); ?>" placeholder="Coppyright" class="regular-text" />
-    <?php
-}
 
 
 
@@ -36,3 +14,26 @@ function my_custom_settings_menu() {
     );
 }
 add_action('admin_menu', 'my_custom_settings_menu');
+
+function settings_api() {
+    add_settings_field('footer', 'Footer', 'footer_func', 'Header');
+    add_settings_field('coppy', 'Coppyright', 'coppy_func', 'Header');
+    
+    register_setting('Header','footer');
+    register_setting('Header','coppy');
+    
+    }
+    add_action('admin_init','settings_api');
+
+    
+function footer_func(){
+    ?>
+    <input type="text" name="footer" value="<?php echo get_option('footer'); ?>" placeholder="Footer" class="regular-text" />
+    <?php
+}
+
+function coppy_func(){
+    ?>
+    <input type="text" name="coppy" value="<?php echo get_option('coppy'); ?>" placeholder="Coppyright" class="regular-text" />
+    <?php
+}
