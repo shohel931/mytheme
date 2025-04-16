4eae948e17b01a89e0b259b29aa7eccfaba108f2
MD. Shohel Rana <shohel065931@gmail.com>
2025-04-16 11:57:09 +0600

    hd

--- template/contact_page.php
+++ template/contact_page.php
@@ -71,7 +71,7 @@ get_header(); ?>
 
 <section id="service_area4">
     <div class="serv_title">
-    <h3><?php echo get_field('title'); ?></h3>
+    <h3><?php echo get_field('title1'); ?></h3>
     </div>
     <div class="serv_main_bx">
         <div class="sub_serv_bx">
@@ -81,9 +81,9 @@ get_header(); ?>
             <p><?php echo get_field('box_1_description'); ?></p>
         </div>
         <div class="sub_serv_bx">
-        <?php $image= get_field('box_2_image'); ?>
+        <?php $image= get_field('box_2_image_'); ?>
         <img src="<?php echo $image; ?>" alt="">
-            <h4><?php echo get_field('box_2_title_'); ?></h4>
+            <h4><?php echo get_field('box_2_title'); ?></h4>
             <p><?php echo get_field('box_2_description'); ?></p>
         </div>
         <div class="sub_serv_bx">
